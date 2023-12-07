90a4b0e0d891fa75906f87e459d7d0ad79e40aa8
Jumcee <futhmah456@gmail.com>
2023-12-07 13:48:18 +0100

    erc1155

--- erc1155-contract/hardhat.config.js
+++ erc1155-contract/hardhat.config.js
@@ -1,9 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
 require('dotenv').config();
 
-const { PRIVATE_KEY, API_URL } = process.env
+const { PRIVATE_KEY, API_URL } = process.env;
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.20",
-};
+  defaultNetwork: "sepolia",
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: process.env.API_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  },
+}
